bca8cf65b3d506e58dbb248c0fb27f59962988ce
FRIEDparrot <814680344@qq.com>
2026-07-26 15:30:34 +0800

    updated training parameters,  added tutorials

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fire-case-detection
-the detection 
+the detection of fire dataset  
+
+### 1. How to use 
+
+1. copy all the dataset train images to `images/all-images`
+2. run `yolo26/scripts/convert_coco_to_yolo.py`, this will generate `train` and `val` folder under the images/all-images
+3. run `yolo26/scripts/train_fire_detection.py` to train the model
--- yolo26/scripts/train_fire_detection.py
+++ yolo26/scripts/train_fire_detection.py
@@ -1,17 +1,16 @@
 import os
 from ultralytics import YOLO
-from ultralytics.utils import YAML
 from utils import ROOT
+from ultralytics.utils import YAML
 
 DATASET_YAML = ROOT / "yolo26" / "dataset.yaml"
 
 def main() -> None:
+    model = YOLO("yolo26m.pt")
+    model.train(data=DATASET_YAML)
     config = YAML.load(DATASET_YAML)
     train_args = dict(config["training"])
-    model_name = train_args.pop("model")
-    train_args["data"] = str(DATASET_YAML)
-    YOLO(model_name).train(**train_args)
-
+    model.train(data=str(DATASET_YAML), **train_args)
 
 if __name__ == "__main__":
     main()
